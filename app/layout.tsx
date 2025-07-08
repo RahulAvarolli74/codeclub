3f5e217d174be8b88bcad7f75cf5d070113950f4
Shivanand-hulikatti <01fe23bcs268@kletech.ac.in>
2025-07-08 11:59:17 +0530

    auth checks

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Bricolage_Grotesque, Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+export const fontSans = Inter({
   subsets: ["latin"],
-});
+})
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+export const fontHeading = Bricolage_Grotesque({
   subsets: ["latin"],
 });
 
@@ -25,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${fontSans.className} antialiased`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,35 @@
-import Image from "next/image";
 import Link from "next/link";
+import { fontHeading } from "./layout";
+import { Button } from "@/components/ui/button";
+import { redirect } from "next/navigation";
+import { auth } from "@/auth";
+import SignOut from "./components/sign-out";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth()
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div>
-       <Link href={'/login'} >
-        Login
-       </Link>
+      <div className="text-center">
+        <div className={`text-7xl ${fontHeading.className}`}>
+          Heil Codeclub
+        </div>
+        {
+          session ? (
+            <div>
+              <div className="text-2xl">
+                Welcome back, {session?.user?.name}!
+              </div>
+                <SignOut />
+            </div>
+          ) : (
+            <Link href="/login" className="text-2xl">
+            <Button>
+              Login
+            </Button>
+            </Link>
+          )
+        }
+        
       </div>
     </div>
   );
