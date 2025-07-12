3c8b617ccdb6280925ef29c5afb63f9996152547
Shivanand-hulikatti <01fe23bcs268@kletech.ac.in>
2025-07-12 20:24:54 +0530

    another check

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,8 @@
+
 import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
-  return new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-  });
+  return new PrismaClient();
 };
 
 declare global {
@@ -14,6 +13,4 @@ const db = globalThis.prisma ?? prismaClientSingleton();
 
 export default db;
 
-if (process.env.NODE_ENV !== "production") {
-  globalThis.prisma = db;
-}
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
