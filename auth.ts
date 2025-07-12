fae8cc8dc96b5d5213b2f4177dbee6b0fd9b89e4
Shivanand-hulikatti <01fe23bcs268@kletech.ac.in>
2025-07-12 20:32:13 +0530

    another one

--- auth.ts
+++ auth.ts
@@ -15,11 +15,26 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
  callbacks: {
   async jwt({ token, user }) {
    if (user) {
-    // get user from db with the email
-    // if there is no user with the email, create new user
-    // else set the user data to token
-    token.id = user.id;
-    token.cfHandle = user.cfHandle || null;
+    // When user first signs in, get their data from the database
+    try {
+      const dbUser = await db.user.findUnique({
+        where: { email: user.email! },
+        select: { id: true, cfHandle: true, email: true, name: true }
+      });
+      
+      if (dbUser) {
+        token.id = dbUser.id;
+        token.cfHandle = dbUser.cfHandle;
+      } else {
+        // User doesn't exist in our database yet
+        token.id = user.id;
+        token.cfHandle = null;
+      }
+    } catch (error) {
+      console.error("Error fetching user from database:", error);
+      token.id = user.id;
+      token.cfHandle = null;
+    }
    }
 
    return token;
@@ -31,21 +46,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     session.user.id = String(token.id);
     session.user.cfHandle = (token.cfHandle as string) || null;
    }
-   if (token.sub) {
-      try {
-        const dbUser = await db.user.findUnique({
-          where: { id: token.sub },
-          select: { id: true, cfHandle: true, email: true, name: true }
-        });
-        
-        if (dbUser) {
-          token.id = dbUser.id;
-          token.cfHandle = dbUser.cfHandle;
-        }
-      } catch (error) {
-        console.error("Error fetching user from database:", error);
-      }
-    }
 
    return session;
   },
